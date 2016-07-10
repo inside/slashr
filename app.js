bce8d4b8ee897c289f8da0cf349a11041d3546e3
Yann Thomas-Gérard <inside@gmail.com>
2016-07-10 22:56:36 +0200

    Adds article

--- app.js
+++ app.js
@@ -1,37 +1,75 @@
-var Image = function(props) {
+var Article = function(props) {
   return (
-    <img
-      src={props.image}
-      onClick={props.onClick}
-      style={{width: '250px'}}
-    />
+    <div
+      className="article"
+      style={{clear: 'both'}}
+    >
+      <img
+        src={props.image_url}
+        onClick={props.handleClick}
+        style={{width: '450px', float: 'left'}}
+      />
+      <h2>
+        {props.title}
+      </h2>
+    </div>
+  )
+}
+
+var ArticleList = function(props) {
+  return (
+    <div className="article-list">
+      <div>
+      {
+        props.articles.map(function(article) {
+          return (
+            <Article
+              {...article}
+              key={article.id}
+              onClick={props.handleClick}
+            />
+          )
+        }, this)
+      }
+      </div>
+      <div>
+        Total: {props.total}
+      </div>
+    </div>
   )
 }
 
 var Slashr = React.createClass({
-  images: [
-    'http://67.media.tumblr.com/13864b31795b1a52a50a04866de39c4e/tumblr_o9u9s5qM8g1r539hzo1_500.jpg',
-    'http://66.media.tumblr.com/56a7e7ecf964a7b6c148ac0e46f8c56f/tumblr_o9rdpwT7aw1r539hzo1_500.jpg',
-    'http://65.media.tumblr.com/8ea2de397f19b9520d98da4bb1494aa9/tumblr_o9mxwgNjHo1r539hzo1_500.jpg',
-    'http://66.media.tumblr.com/b85637e9aa950379769a98f27ba09a6b/tumblr_o9j2iqtf0C1r539hzo1_500.jpg'
-  ],
-
   getRandomIntInclusive: function(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min
   },
 
   handleClick: function() {
-    var index = this.getRandomIntInclusive(0, this.images.length - 1)
+    console.log('handleClick called')
+    //var index = this.getRandomIntInclusive(0, this.articles.length - 1)
 
-    this.setState({
-      image: this.images[index]
-    })
+    //this.setState({
+      //image: this.articles[index]
+    //})
   },
 
   getInitialState: function() {
     return {
-      image: this.images[0]
+      articles: []
     }
+  },
+
+  componentDidMount: function() {
+    var that = this
+
+    fetch('/slashr/page1.json').then(function(response) {
+        response.json().then(function(json) {
+          that.setState({
+            articles: json.data,
+            total: json.total
+          })
+        })
+      })
   },
 
   render: function() {
@@ -40,15 +78,13 @@ var Slashr = React.createClass({
         <h1>
           Hello, this is Slashr!
         </h1>
-        <Image
-          image={this.state.image}
-          onClick={this.handleClick}
+        <ArticleList
+          articles={this.state.articles}
+          total={this.state.total}
         />
       </div>
     )
   }
 })
 
-ReactDOM.render(
-    <Slashr/>,
-    document.getElementById('root'))
+ReactDOM.render(<Slashr/>, document.getElementById('root'))
